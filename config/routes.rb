d5136d9f3495b73527088c5afcd9883f935b9f2e
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-06 17:36:49 -0300

    profile traits

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :profile_traits, except: :destroy
+  resources :profile_traits, only: %i[create update]
 
   resources :containers, except: %i[index show destroy] do
     resources :cards do
