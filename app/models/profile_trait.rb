1dab86b0ab7d8ecee86e3c8b78ef420312998083
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-06 16:19:30 -0300

    profile_traits validations

--- app/models/profile_trait.rb
+++ app/models/profile_trait.rb
@@ -1,4 +1,6 @@
 class ProfileTrait < ApplicationRecord
+  validates :user_answer, presence: true
+
   belongs_to :user
   belongs_to :trait
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+
+  has_many :profile_traits, dependent: :destroy
+  has_many :containers, dependent: :destroy
+  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
