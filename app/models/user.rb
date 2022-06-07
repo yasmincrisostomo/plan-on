cd682198ee2b659cf8204f3558da04a494d387c6
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-07 18:55:01 -0300

    corrigi erro dos profiles traits

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
+  after_create :create_profiles_traits
+
   has_many :profile_traits, dependent: :destroy
   accepts_nested_attributes_for :profile_traits
 
@@ -9,4 +11,10 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def create_profiles_traits
+    Trait.all.each do |trait|
+      ProfileTrait.create!(user: self, trait: trait, user_answer: 0)
+    end
+  end
 end
