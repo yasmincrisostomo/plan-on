e823c5a36b40ce5a7b27f3cb20492f476207e434
Pedro Henrique Costa <pedrocosta99@gmail.com>
2022-06-07 18:04:04 -0300

    card form added

--- app/javascript/controllers/kanban_controller.js
+++ app/javascript/controllers/kanban_controller.js
@@ -13,7 +13,27 @@ export default class extends Controller {
       new Sortable(card, {
           group: 'kanban', // set both lists to same group
           animation: 300,
-          onEnd: this.saveKanban
+          onEnd: this.saveKanban,
+          store: {
+            /**
+             * Get the order of elements. Called once during initialization.
+             * @param   {Sortable}  sortable
+             * @returns {Array}
+             */
+            get: function (sortable) {
+              var order = localStorage.getItem(sortable.options.group.name);
+              return order ? order.split('|') : [];
+            },
+
+            /**
+             * Save the order of elements. Called onEnd (when the item is dropped).
+             * @param {Sortable}  sortable
+             */
+            set: function (sortable) {
+              var order = sortable.toArray();
+              localStorage.setItem(sortable.options.group.name, order.join('|'));
+            }
+          }
       });
     });
 
