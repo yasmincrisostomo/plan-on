6727f31a05eb7c663f7e94ecbfa57a520755c693
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-08 12:08:54 -0300

    profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,12 @@ Trait.create!(name: "Equilíbrio emocional", message: "Melhore!")
 
 Trait.create!(name: "Qualidade do material", message: "Melhore!")
 
-User.create!(email: 'alguem@email.com', password: '123123', name: 'Alguem', type_of_student: 'Folgado')
+user = User.create!(email: 'alguem@email.com', password: '123123', name: 'Alguem', type_of_student: 'Folgado')
 
-Container.create!(card_category: "To be", user_id: 1)
-Container.create!(card_category: "Not to be", user_id: 1)
+container1 = Container.create!(card_category: "To be", user: user)
+container2 = Container.create!(card_category: "Not to be", user: user)
 
-Card.create!(title: "Coisa 1", description: "lalala", position: 1, container_id: 1)
-Card.create!(title: "Coisa 2", description: "lalala", position: 2, container_id: 1)
-Card.create!(title: "Coisa A", description: "lalala", position: 1, container_id: 2)
-Card.create!(title: "Coisa B", description: "lalala", position: 2, container_id: 2)
+Card.create!(title: "Coisa 1", description: "lalala", position: 1, container: container1)
+Card.create!(title: "Coisa 2", description: "lalala", position: 2, container: container1)
+Card.create!(title: "Coisa A", description: "lalala", position: 1, container: container2)
+Card.create!(title: "Coisa B", description: "lalala", position: 2, container: container2)
