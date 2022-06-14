037a83bdbdc174a76b8d168af6dcb310b3cbe11a
yasmin crisostomo <crisostomyasmin@gmail.com>
2022-06-14 15:03:34 -0300

    cards

---
+++ app/javascript/controllers/bar_controller.js
@@ -0,0 +1,50 @@
+import { Controller } from "stimulus"
+import Chart from 'chart.js/auto';
+
+export default class extends Controller {
+  static values = {
+    values: Object
+  }
+  static targets = ["canvas"]
+
+  connect() {
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+    };
+    const labels = Utils.months({count: 7});
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)'
+        ],
+        borderWidth: 1
+      }]
+    };
+  }
+}
